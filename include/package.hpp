6282af16c73535d52910ebb290cc76553e125e42
Tomasz Mazur <toptomek05@gmail.com>
2026-01-15 19:10:28 +0100

    uzupelniono package w hpp

--- include/package.hpp
+++ include/package.hpp
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "types.hpp"
+#include <set>
 
 
 namespace NetSim 
@@ -10,7 +11,26 @@ namespace NetSim
 
     class Package 
     {
-        
+        public:
+            Package();
+
+            explicit Package(ElementID id);
+
+            Package(const Package &) = delete;
+            Package &operator=(const Package &) = delete;
+
+            Package(Package &&) noexcept;
+            Package &operator=(Package &&) noexcept;
+
+            ElementID get_id() const;
+
+            ~Package();
+
+        private:
+            ElementID id_;
+
+            static std::set<ElementID> assigned_ids_;
+            static std::set<ElementID> freed_ids_;        
     };
 
 }
