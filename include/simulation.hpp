13dc2b773a0b71475991f0902ac18c7f47433d47
Pmrowka25 <mrowkpio7@gmail.com>
2026-01-15 23:22:02 +0100

    Funkcjonalność symulacji uzupełniona

--- include/simulation.hpp
+++ include/simulation.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 #include "factory.hpp"
-
+#include <functional>
+#include <set>
 
 namespace NetSim 
 {
@@ -9,11 +10,29 @@ namespace NetSim
 
     class IntervalReportNotifier 
     {
-        
+        public:
+            explicit IntervalReportNotifier(TimeOffset to): 
+                to_(to) 
+            {}
+
+            bool should_generate_report(Time t) const 
+            { return (t - 1) % to_ == 0; }
+
+        private:
+            TimeOffset to_;
     };
 
     class SpecificTurnsReportNotifier 
     {
-        
+        public:
+            explicit SpecificTurnsReportNotifier(std::set<Time> turns): 
+                turns_(std::move(turns)) 
+            {}
+
+            bool should_generate_report(Time t) const 
+            { return turns_.find(t) != turns_.end(); }
+
+        private:
+            std::set<Time> turns_;
     };
 } 
