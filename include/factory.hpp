1377f6aaee6ad5952669d22f59bdf1d4a9d01418
Tomasz Mazur <toptomek05@gmail.com>
2026-01-15 20:08:28 +0100

    ...NodeCollection i ParsedLineData w hpp

--- include/factory.hpp
+++ include/factory.hpp
@@ -12,7 +12,56 @@ namespace NetSim
     template <typename Node>
     class NodeCollection 
     {
-        
+        public:
+            using container_t = std::list<Node>;
+            using iterator = typename container_t::iterator;
+            using const_iterator = typename container_t::const_iterator; 
+                                                    
+            //-----------------------------------------------------------------------------------
+
+            iterator begin() { return container_.begin(); }
+            iterator end() { return container_.end(); }
+
+            const_iterator begin() const { return container_.begin(); }
+            const_iterator end() const { return container_.end(); }
+            
+            const_iterator cbegin() const { return container_.cbegin(); }
+            const_iterator cend() const { return container_.cend(); }
+
+            //-----------------------------------------------------------------------------------
+
+            void add(Node &&node) 
+            { container_.emplace_back(std::move(node)); }
+
+
+            void remove_by_id(ElementID id) 
+            {
+                auto it = find_by_id(id);
+                if (it != container_.end()) 
+                { container_.erase(it); }
+            }
+
+            //-----------------------------------------------------------------------------------
+
+            iterator find_by_id(ElementID id) 
+            {
+                return std::find_if(
+                    container_.begin(), container_.end(),
+                    [id](const Node &node) { return node.get_id() == id; });
+            }
+
+
+            const_iterator find_by_id(ElementID id) const 
+            {
+                return std::find_if(
+                    container_.cbegin(), container_.cend(),
+                    [id](const Node &node) { return node.get_id() == id; });
+            }
+
+            //-----------------------------------------------------------------------------------
+
+        private:
+            container_t container_;        
     };
 
     //-----------------------------------------------------------------------------------
@@ -29,7 +78,8 @@ namespace NetSim
 
     struct ParsedLineData 
     {
-        
+        ElementType element_type;
+        std::map<std::string, std::string> parameters;        
     };
 
     ParsedLineData parse_line(std::string line);
